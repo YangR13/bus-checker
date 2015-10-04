a8e9d2ecd7a3a70681f5d2371a9dab2865225098
YangR13 <yangren@sbcglobal.net>
2015-10-03 22:19:31 -0700

    JSON support added
    addBusStop now accepts JSON strings for bus stop data

--- script.js
+++ script.js
@@ -1,61 +1,39 @@
-function loadData() {
+function loadData(jsonData) {
+	addAgencyName(jsonData.agency_name);
+	for(var i=0; i<jsonData.stops.length; i++) {
+		addBusStop(jsonData.stops[i]);
+	}
+}
+
+function addAgencyName(agencyName) {
 	
 }
 
-function addAgencyName(agencyNameString) {
-	
-}
-
-function addBusStop(busStopString) {
-	
-	var busStopName = 'The Moon';
-	var busDirectionName1 = 'Direction A';
-	var busDirectionName2 = 'Direction B';
-	
-	var stopIndex = busStopString.indexOf("stop:")+6;
-	var routeIndicies;
-	var timeIndicies;
-	
-	
-	busStopName = busStopString.substr();
-	
-	
-	var busLineString;
-	
-	// parse busLineString here
-	
-	var busLineArray1 = ["", "", ""];	// Direction 1
-	var busLineArray2 = ["", "", ""];	// Direction 2
+function addBusStop(stopData) {
 	
 	var busStopList = document.getElementById("bus-stop-list");
 	var busStop = document.createElement("DIV");
 	busStop.className = "bus-stop";
-	busStop.innerHTML = '<span class="bus-stop-name">'+busStopName+'</span><div class="bus-direction"><span class="bus-direction-name">'+busDirectionName1+'</span></div><div class="bus-direction"><span class="bus-direction-name">'+busDirectionName2+'</span></div>';
+	busStop.innerHTML = '<span class="bus-stop-name">'+stopData.stop_name+'</span><div class="bus-direction"><span class="bus-direction-name">'+stopData.directions[0].direction_name+'</span></div><div class="bus-direction"><span class="bus-direction-name">'+stopData.directions[1].direction_name+'</span></div>';
 	
-	for(var i=0; i<busLineArray1.length; i++) {
-		var busLineColor = "red";
-		var busLineName = "1000";
-		var busLineTimes = ["10:00AM", "10:15AM", "10:30AM"];
+	for(var dir=0; dir<2; dir++) {
+			
+		for(var i=0; i<stopData.directions[dir].routes.length; i++) {
+			var route = stopData.directions[dir].routes[i];
 		
-		var busDirection1 = busStop.childNodes[1];
-		var busLine = document.createElement("DIV");
-		busLine.className = "bus-line";
-		busLine.innerHTML = '<span class="bus-line-name" style="background-color:'+busLineColor+'">'+busLineName+'</span><span class="bus-line-time">'+busLineTimes[0]+'</span><span class="bus-line-time">'+busLineTimes[1]+'</span><span class="bus-line-time">'+busLineTimes[2]+'</span>';
+			var busDirection = busStop.childNodes[dir+1];
+			var busLine = document.createElement("DIV");
+			busLine.className = "bus-line";
+			busLine.innerHTML = '<span class="bus-line-name" style="background-color:'+route.route_color+'">'+route.route_short_name+'</span><span class="bus-line-time">'+route.arrival_times[0].time+'</span><span class="bus-line-time">'+route.arrival_times[1].time+'</span><span class="bus-line-time">'+route.arrival_times[2].time+'</span>';
 		
-		busDirection1.appendChild(busLine);
+			busDirection.appendChild(busLine);
+		}
 	}
-	for(var i=0; i<busLineArray2.length; i++) {
-		var busLineColor = "blue";
-		var busLineName = "2000";
-		var busLineTimes = ["10:00AM", "10:15AM", "10:30AM"];
-		
-		var busDirection2 = busStop.childNodes[2];
-		var busLine = document.createElement("DIV");
-		busLine.className = "bus-line";
-		busLine.innerHTML = '<span class="bus-line-name" style="background-color:'+busLineColor+'">'+busLineName+'</span><span class="bus-line-time">'+busLineTimes[0]+'</span><span class="bus-line-time">'+busLineTimes[1]+'</span><span class="bus-line-time">'+busLineTimes[2]+'</span>';
-		
-		busDirection2.appendChild(busLine);
-	}
-	
 	busStopList.appendChild(busStop);
+}
+
+function addBusStopTest() {
+	var text = '{"stop_name": "UCSB Bus Loop","stop_distance": "10","directions": [{"direction_id": "0","direction_name": "North","routes": [{"route_short_name": "24x","route_id": "1","route_color": "blue","trip_headsign": "Marketplace","arrival_times": [{"time":"10:00AM"},{"time":"11:00AM"},{"time":"12:00AM"}]},{"route_short_name": "11","route_id": "2","route_color": "red","trip_headsign": "Marketplace","arrival_times": [{"time":"10:00AM"},{"time":"11:00AM"},{"time":"12:00AM"}]}]},{"direction_id": "1","direction_name": "South","routes": [{"route_short_name": "24x","route_id": "1","route_color": "blue","trip_headsign": "Marketplace","arrival_times": [{"time":"10:00AM"},{"time":"11:00AM"},{"time":"12:00AM"}]},{"route_short_name": "11","route_id": "2","route_color": "red","trip_headsign": "Marketplace","arrival_times": [{"time":"10:00AM"},{"time":"11:00AM"},{"time":"12:00AM"}]}]}]}';
+	var stopData = JSON.parse(text);
+	addBusStop(stopData);
 }
