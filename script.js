69a6c46a94b55b2bc03c113d304e7fb6732d212c
YangR13 <yangren@sbcglobal.net>
2015-10-03 22:52:36 -0700

    Added agency name
    - Agency name is displayed on header
    - Slight change to JSON format + sample

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function loadData(jsonData) {
 }
 
 function addAgencyName(agencyName) {
-	
+	document.getElementById("agency-name").innerHTML = agencyName;
 }
 
 function addBusStop(stopData) {
@@ -24,7 +24,7 @@ function addBusStop(stopData) {
 			var busDirection = busStop.childNodes[dir+2];
 			var busLine = document.createElement("DIV");
 			busLine.className = "bus-line";
-			busLine.innerHTML = '<span class="bus-line-name" style="background-color:'+route.route_color+'">'+route.route_short_name+'</span><span class="bus-line-time">'+route.arrival_times[0].time+'</span><span class="bus-line-time">'+route.arrival_times[1].time+'</span><span class="bus-line-time">'+route.arrival_times[2].time+'</span>';
+			busLine.innerHTML = '<span class="bus-line-name" style="background-color:'+route.route_color+'">'+route.route_short_name+'</span><span class="bus-line-time">'+route.arrival_times[0]+'</span><span class="bus-line-time">'+route.arrival_times[1]+'</span><span class="bus-line-time">'+route.arrival_times[2]+'</span>';
 		
 			busDirection.appendChild(busLine);
 		}
@@ -36,4 +36,10 @@ function addBusStopTest() {
 	var text = '{"stop_name": "UCSB Bus Loop","stop_distance": "10","directions": [{"direction_id": "0","direction_name": "North","routes": [{"route_short_name": "24x","route_id": "1","route_color": "blue","trip_headsign": "Marketplace","arrival_times": [{"time":"10:00AM"},{"time":"11:00AM"},{"time":"12:00AM"}]},{"route_short_name": "11","route_id": "2","route_color": "red","trip_headsign": "Marketplace","arrival_times": [{"time":"10:00AM"},{"time":"11:00AM"},{"time":"12:00AM"}]}]},{"direction_id": "1","direction_name": "South","routes": [{"route_short_name": "24x","route_id": "1","route_color": "blue","trip_headsign": "Marketplace","arrival_times": [{"time":"10:00AM"},{"time":"11:00AM"},{"time":"12:00AM"}]},{"route_short_name": "11","route_id": "2","route_color": "red","trip_headsign": "Marketplace","arrival_times": [{"time":"10:00AM"},{"time":"11:00AM"},{"time":"12:00AM"}]}]}]}';
 	var stopData = JSON.parse(text);
 	addBusStop(stopData);
+}
+
+function loadDataTest() {
+	var text = '{"agency_name":"MTD", "stops": [{"stop_name": "UCSB Bus Loop","stop_distance": "10","directions": [{"direction_id": "0","direction_name": "North","routes": [{"route_short_name": "24x","route_id": "1","route_color": "blue","trip_headsign": "Marketplace","arrival_times": ["10:00AM", "11:00AM", "12:00AM"]},{"route_short_name": "11","route_id": "2","route_color": "red","trip_headsign": "Marketplace","arrival_times": ["10:00AM", "11:00AM", "12:00AM"]}]},{"direction_id": "1","direction_name": "South","routes": [{"route_short_name": "24x","route_id": "1","route_color": "blue","trip_headsign": "Marketplace","arrival_times": ["10:00AM", "11:00AM", "12:00AM"]},{"route_short_name": "11","route_id": "2","route_color": "red","trip_headsign": "Marketplace","arrival_times": ["10:00AM", "11:00AM", "12:00AM"]}]}]},{"stop_name": "Sierra Madre","stop_distance": "2000","directions": [{"direction_id": "0","direction_name": "North","routes": [{"route_short_name": "24x","route_id": "1","route_color": "blue","trip_headsign": "Marketplace","arrival_times": ["10:00AM", "11:00AM", "12:00AM"]},{"route_short_name": "11","route_id": "2","route_color": "red","trip_headsign": "Marketplace","arrival_times": ["10:00AM", "11:00AM", "12:00AM"]}]},{"direction_id": "1","direction_name": "South","routes": [{"route_short_name": "24x","route_id": "1","route_color": "blue","trip_headsign": "Marketplace","arrival_times": ["10:00AM", "11:00AM", "12:00AM"]},{"route_short_name": "11","route_id": "2","route_color": "red","trip_headsign": "Marketplace","arrival_times": ["10:00AM", "11:00AM", "12:00AM"]}]}]}]}';
+	var jsonData = JSON.parse(text);
+	loadData(jsonData);
 }
